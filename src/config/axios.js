e879f787e7428dc43a6fad996c222da872f46f59
kirit <kiriit@nimblechapps.com>
2021-06-21 08:11:08 +0530

    Fixed

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,18 +1,22 @@
-import axios from "axios"
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://api-bennybite.herokuapp.com/v1',
+    // baseURL: 'https://api-bennybite.herokuapp.com/v1',
+    baseURL: "https://api-foodevent.herokuapp.com/v1",
 });
 
 // Add a response interceptor
-instance.interceptors.response.use(function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response.data;
-}, function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    return Promise.reject(error);
-});
+instance.interceptors.response.use(
+    function (response) {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response.data;
+    },
+    function (error) {
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+        return Promise.reject(error);
+    }
+);
 
-export default instance
+export default instance;
--- src/layouts/Profile/index.js
+++ src/layouts/Profile/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import AsyncStorage from "@react-native-community/async-storage";
-import { StackActions, NavigationActions } from "react-navigation"
+import { StackActions, NavigationActions } from "react-navigation";
 import { connect } from "react-redux";
 import { get } from "lodash";
 import {
-  View,
-  SafeAreaView,
-  Text,
-  TouchableOpacity,
-  Image,
+    View,
+    SafeAreaView,
+    Text,
+    TouchableOpacity,
+    Image,
 } from "react-native";
 import { authOperations } from "./../../state/ducks/auth";
 import HeaderTitle from "../../components/Header/HeaderTitle";
@@ -17,92 +17,105 @@ import styles from "./styles";
 import { Globals } from "../../utils/variable";
 
 export class Profile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  static navigationOptions = ({ navigation }) => {
-    return {
-      headerTitle: () => <HeaderTitle title={"Settings"} />,
-      headerTitle: () => <HeaderTitle title={"Settings"} />,
-      headerRight: (
-        <HeaderRight
-          buttonTitle="Edit"
-          onPress={() => {
-            navigation.navigate("EditProfile");
-          }}
-        />
-      ),
-    };
-  };
-
-  render() {
-    const { user } = this.props;
-    let pic = require('../../assets/Images/user.png');
-    if (user.pic)
-      pic = { uri: user.pic }
-
-    return (
-      <View style={styles.safeareaview}>
-        <SafeAreaView style={styles.contentcenter}>
-          <View style={styles.centeredView}>
-            <View style={styles.profilecontentcenter}>
-              <View style={styles.profilecontent}>
-                <Text style={styles.prfltxt}>User Profile</Text>
-                <TouchableOpacity style={styles.profileview}>
-                  <Image style={styles.profilepic} source={pic} />
-                </TouchableOpacity>
-                <Text style={styles.usertxt}>{user.name}</Text>
-                <Text style={styles.emailtxt}>{user.email}</Text>
-                <View style={styles.bottomtxt}>
-                  <View style={styles.line}></View>
-                  <TouchableOpacity
-                    onPress={() => {
-                      this.props.navigation.navigate("ChangePassword");
-                    }}
-                  >
-                    <Text style={styles.chnpasstxt}>Change password</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    onPress={this.onLogout}>
-                    <Text style={styles.logouttxt}>Logout</Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
-            </View>
-          </View>
-        </SafeAreaView>
-      </View>
-    );
-  }
-
-  onLogout = async () => {
-    try {
-      const device_token = await AsyncStorage.getItem(Globals.kDeviceToken);
-      const device_type = await AsyncStorage.getItem(Globals.kDeviceType);
-      // const device_token = 'dddc1d378d18cc562e61d4951536af719c9480abea10bd8c5b234253f1f3803e';
-      // const device_type = 'ios';
-      await this.props.logout({
-        device_type
-      });
-      await AsyncStorage.clear()
-      await AsyncStorage.setItem(Globals.kDeviceToken, device_token);
-      await AsyncStorage.setItem(Globals.kDeviceType, device_type);
-      const logoutAction = StackActions.reset({
-        index: 0,
-        actions: [NavigationActions.navigate({ routeName: 'Authenticate' })],
-      });
-      this.props.navigation.dispatch(logoutAction);
-      // this.props.navigation.navigate("Login");
-    } catch (err) {
-      console.log("Errror", err);
+    constructor(props) {
+        super(props);
+        this.state = {};
     }
-  };
+
+    static navigationOptions = ({ navigation }) => {
+        return {
+            headerTitle: () => <HeaderTitle title={"Settings"} />,
+            headerTitle: () => <HeaderTitle title={"Settings"} />,
+            headerRight: (
+                <HeaderRight
+                    buttonTitle="Edit"
+                    onPress={() => {
+                        navigation.navigate("EditProfile");
+                    }}
+                />
+            ),
+        };
+    };
+
+    render() {
+        const { user } = this.props;
+        let pic = require("../../assets/Images/user.png");
+        if (user.pic) pic = { uri: user.pic };
+
+        return (
+            <View style={styles.safeareaview}>
+                <SafeAreaView style={styles.contentcenter}>
+                    <View style={styles.centeredView}>
+                        <View style={styles.profilecontentcenter}>
+                            <View style={styles.profilecontent}>
+                                <Text style={styles.prfltxt}>User Profile</Text>
+                                <TouchableOpacity style={styles.profileview}>
+                                    <Image
+                                        style={styles.profilepic}
+                                        source={pic}
+                                    />
+                                </TouchableOpacity>
+                                <Text style={styles.usertxt}>{user.name}</Text>
+                                <Text style={styles.emailtxt}>
+                                    {user.email}
+                                </Text>
+                                <View style={styles.bottomtxt}>
+                                    <View style={styles.line}></View>
+                                    <TouchableOpacity
+                                        onPress={() => {
+                                            this.props.navigation.navigate(
+                                                "ChangePassword"
+                                            );
+                                        }}
+                                    >
+                                        <Text style={styles.chnpasstxt}>
+                                            Change password
+                                        </Text>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity onPress={this.onLogout}>
+                                        <Text style={styles.logouttxt}>
+                                            Logout
+                                        </Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                </SafeAreaView>
+            </View>
+        );
+    }
+
+    onLogout = async () => {
+        try {
+            const device_token = await AsyncStorage.getItem(
+                Globals.kDeviceToken
+            );
+            const device_type = await AsyncStorage.getItem(Globals.kDeviceType);
+            // const device_token = 'dddc1d378d18cc562e61d4951536af719c9480abea10bd8c5b234253f1f3803e';
+            // const device_type = 'ios';
+            await this.props.logout({
+                device_type,
+            });
+            await AsyncStorage.clear();
+            await AsyncStorage.setItem(Globals.kDeviceToken, device_token);
+            await AsyncStorage.setItem(Globals.kDeviceType, device_type);
+            const logoutAction = StackActions.reset({
+                index: 0,
+                actions: [
+                    NavigationActions.navigate({ routeName: "Authenticate" }),
+                ],
+            });
+            this.props.navigation.dispatch(logoutAction);
+            // this.props.navigation.navigate("Login");
+        } catch (err) {
+            console.log("Errror", err);
+        }
+    };
 }
 
 const mapStateToProps = (state) => ({
-  user: get(state, "auth.session.user", {}),
+    user: get(state, "auth.session.user", {}),
 });
 
 const mapDispatchToProps = { logout: authOperations.logout };
